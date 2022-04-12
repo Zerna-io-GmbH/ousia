eda2a53825311803bb4fa303c941a741d3f9757e
zerna.io <michael@zerna.io>
2022-04-12 19:11:53 +0200

    feat(zerna.io): add skeleton files for zerna.io landing page
    - add favicons :-)
    - build basic structure add create graphcms (of cource config is hidden)
    - Add basic navigation example from tailwindUI
    - add first layout sections for the landing page with an clear TailwindUI overhead
    - delete unused boilerplate from application template

---
+++ packages/zerna.io/src/lib/components/Logo.svelte
@@ -0,0 +1,230 @@
+<script>
+	export let strokeColor = 'black';
+</script>
+
+<svg
+	height="100%"
+	stroke-miterlimit="10"
+	version="1.1"
+	viewBox="0 0 500 500"
+	width="100%"
+	xml:space="preserve"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+>
+	<defs />
+	<g id="Circles">
+		<path
+			d="M217.557 69.9103C217.557 52.409 231.745 38.2214 249.246 38.2214C266.747 38.2214 280.935 52.409 280.935 69.9103C280.935 87.4117 266.747 101.599 249.246 101.599C231.745 101.599 217.557 87.4117 217.557 69.9103Z"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M410.48 210.437C410.48 192.935 424.668 178.748 442.169 178.748C459.671 178.748 473.858 192.935 473.858 210.437C473.858 227.938 459.671 242.126 442.169 242.126C424.668 242.126 410.48 227.938 410.48 210.437Z"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M276.615 186.026C276.615 168.525 290.803 154.337 308.304 154.337C325.806 154.337 339.993 168.525 339.993 186.026C339.993 203.527 325.806 217.715 308.304 217.715C290.803 217.715 276.615 203.527 276.615 186.026Z"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M158.893 186.813C158.893 169.312 173.08 155.124 190.582 155.124C208.083 155.124 222.271 169.312 222.271 186.813C222.271 204.315 208.083 218.502 190.582 218.502C173.08 218.502 158.893 204.315 158.893 186.813Z"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M122.277 301.386C122.277 283.885 136.464 269.697 153.966 269.697C171.467 269.697 185.655 283.885 185.655 301.386C185.655 318.888 171.467 333.075 153.966 333.075C136.464 333.075 122.277 318.888 122.277 301.386Z"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M314.019 301.78C314.019 284.279 328.206 270.091 345.708 270.091C363.209 270.091 377.397 284.279 377.397 301.78C377.397 319.281 363.209 333.469 345.708 333.469C328.206 333.469 314.019 319.281 314.019 301.78Z"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M103.772 429.739C103.772 412.238 117.959 398.05 135.461 398.05C152.962 398.05 167.15 412.238 167.15 429.739C167.15 447.241 152.962 461.428 135.461 461.428C117.959 461.428 103.772 447.241 103.772 429.739Z"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M335.673 430.527C335.673 413.025 349.861 398.838 367.362 398.838C384.864 398.838 399.051 413.025 399.051 430.527C399.051 448.028 384.864 462.216 367.362 462.216C349.861 462.216 335.673 448.028 335.673 430.527Z"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M218.738 371.862C218.738 354.361 232.926 340.173 250.427 340.173C267.929 340.173 282.116 354.361 282.116 371.862C282.116 389.364 267.929 403.551 250.427 403.551C232.926 403.551 218.738 389.364 218.738 371.862Z"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M219.132 270.282C219.132 252.781 233.32 238.593 250.821 238.593C268.322 238.593 282.51 252.781 282.51 270.282C282.51 287.784 268.322 301.971 250.821 301.971C233.32 301.971 219.132 287.784 219.132 270.282Z"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M27.3898 210.043C27.3898 192.542 41.5774 178.354 59.0787 178.354C76.5801 178.354 90.7677 192.542 90.7677 210.043C90.7677 227.544 76.5801 241.732 59.0787 241.732C41.5774 241.732 27.3898 227.544 27.3898 210.043Z"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+	</g>
+	<g id="Lines">
+		<path
+			d="M249.803 114.132L250.197 234.611"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M270.662 241.339L283.266 221.701"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M211.365 215.907L229.707 240.684"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M289.88 257.095L402.825 222.021"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M218.766 259.025L99.7074 224.273"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M188.197 291.203L208.791 284.124"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M289.558 283.159L308.543 290.56"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M223.915 305.362L160.846 393.851"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M276.043 304.396L343.939 396.104"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+		<path
+			d="M249.335 312.763L249.335 330.139"
+			fill="none"
+			fill-rule="evenodd"
+			opacity="1"
+			stroke={strokeColor}
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="20.384"
+		/>
+	</g>
+</svg>
