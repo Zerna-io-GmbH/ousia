69b1e4239a63ec0a3fb8e54439b30e99b0a67694
zerna.io <michael@zerna.io>
2022-05-27 20:10:26 +0200

    feat(zerna.io): integrate FullscreenLoader and DotsImpulsive
    - integrate FullscreenLoader with DotsImpulsive
    - update app headers according to svelte new release

--- packages/zerna.io/src/routes/__layout.svelte
+++ packages/zerna.io/src/routes/__layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-	import { OusiaAppLayout } from '@ousia/application-ui/components';
-	import '../app.css';
+	import { FullscreenLoader, OusiaAppLayout } from '@ousia/application-ui/components';
+	import { DotsImpulse } from '@ousia/application-ui/svg'
 	import Navigation from '$lib/components/Navigation.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import '../app.css';
 </script>
 
+<FullscreenLoader className="bg-indigo-50 opacity-25">
+	<DotsImpulse className='h-72 fill-indigo-900' />
+</FullscreenLoader>
 <OusiaAppLayout>
 	<Navigation />
 	<main class="grid">
