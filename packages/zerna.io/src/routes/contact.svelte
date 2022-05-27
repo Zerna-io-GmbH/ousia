020b32e3dab104edf5dbd8321e02ab49bf0e231d
zerna.io <michael@zerna.io>
2022-05-27 20:11:22 +0200

    refactor(zerna.io): remove debug components

--- packages/zerna.io/src/routes/contact.svelte
+++ packages/zerna.io/src/routes/contact.svelte
@@ -284,7 +284,6 @@
 							/>
 						</div>
 						<div class="sm:col-span-2 sm:flex sm:justify-end">
-							<button on:click="{toggleLoading}">Toggle</button>
 							<button
 								type="submit"
 								class="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto"
